6a4166308aecbff88f9c18981d7c97c6fe58ccf9
David Caleb Robinson <calebrob6@gmail.com>
2014-04-08 22:29:21 -0500

    Speed testing on python version

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ def main():
 
     start = time.time()
 
-    width = 600
-    height = 600
+    width = 200
+    height = 200
 
     currentFrame = 0
     currentOffset = 0
@@ -49,9 +49,7 @@ def main():
     stopFlickering = False
     backwards = False
     
-    board = np.zeros((height,width), dtype=np.uint8)
-    nextState = np.zeros((height,width), dtype=np.uint8)
-    oldData = 0
+    board = np.zeros((height*width))
     
     rules = Rules()
     rules.setRules("15;14;13;3;11;5;6;1;7;9;10;2;12;4;8;0")
@@ -61,54 +59,51 @@ def main():
     for i in range(randomNum):
         x = random.randrange(0,width)
         y = random.randrange(0,height)
-        board[y,x] = 255
+        board[((width * ((y)%height)) + ((x)%width))] = 255
 
-    cv2.imshow("test",board)
-    cv2.waitKey()
+    #cv2.imshow("test",board)
+    #if cv2.waitKey(1) & 0xFF == ord('q'):
+    #    pass
 
     while currentFrame<100:
         print currentFrame
-
-        if stopFlickering:
-            if abs(currentFrame%2)==0:
-                pass
-            else:
-                board = oldData
-        else:
-            pass
+        delta = time.time()
 
         for i in range(0,height,2):
             for j in range(0,width,2):
                 x = j+currentOffset
                 y = i+currentOffset
 
-                a1 = board[y%height,x%width] == 255
-                a2 = board[y%height,(x+1)%width] == 255
-                a3 = board[(y+1)%height,x%width] == 255
-                a4 = board[(y+1)%height,(x+1)%width] == 255
+                a1 = board[((width * ((y)%height)) + ((x)%width))] == 255
+                a2 = board[((width * ((y)%height)) + ((x+1)%width))] == 255
+                a3 = board[((width * ((y+1)%height)) + ((x)%width))] == 255
+                a4 = board[((width * ((y+1)%height)) + ((x+1)%width))] == 255
 
+                '''
                 cellIndex = rules.configRuleToIdx[(a1,a2,a3,a4)]
 
                 transitionIndex = rules.rules[cellIndex]
                 transitionConfig = rules.configIdxToRule[transitionIndex]
 
-                nextState[y%height,x%width] = 255;
-                nextState[y%height,(x+1)%width] = 255;
-                nextState[(y+1)%height,x%width] = 255;
-                nextState[(y+1)%height,(x+1)%width] = 255;
+                board[y%height,x%width] = 255;
+                board[y%height,(x+1)%width] = 255;
+                board[(y+1)%height,x%width] = 255;
+                board[(y+1)%height,(x+1)%width] = 255;
+                '''
         #end board update
+        print time.time() - delta
                 
         if stopFlickering:
             if abs(currentFrame%2)==1:
-                board = nextState
-                cv2.imshow("test",board)
-                cv2.waitKey()
-            else:
-                oldData = newState
+                #cv2.imshow("test",board)
+                #if cv2.waitKey(1) & 0xFF == ord('q'):
+                #    break
+                pass
         else:
-            board = nextState
-            cv2.imshow("test",board)
-            cv2.waitKey()
+            #cv2.imshow("test",board)
+            #if cv2.waitKey(1) & 0xFF == ord('q'):
+            #    break
+            pass
 
 
         if backwards:
@@ -118,7 +113,8 @@ def main():
         frameAbs+=1
 
         currentOffset = not currentOffset
-
+        print time.time() - delta
+        
     print("Time taken: %f seconds" % (time.time() - start))
 
 
