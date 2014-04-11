5eed00d43633fe8e904e2100ff5ff136dd8a40d2
calebrob6 <calebrob6@gmail.com>
2014-04-11 10:56:21 -0500

    Still working on java tester

--- evaluateEntropy.m
+++ evaluateEntropy.m
@@ -1,5 +1,7 @@
-I = imread('initial.png'); 
-J = entropy(I)
-
-I = imread('final.png'); 
-J = entropy(I)
+fnames = dir('*.png');
+numfids = length(fnames);
+for K = 1:numfids
+I = imread(fnames(K).name);
+J = entropy(I);
+fprintf('for file %s, entropy %f\n',fnames(K).name,J);
+end
