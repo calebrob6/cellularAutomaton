272fe1884fb5a55d687c9301b3c768a85ba3168e
calebrob6 <calebrob6@gmail.com>
2014-04-12 18:23:51 -0500

    Working

--- analyze.py
+++ analyze.py
@@ -7,13 +7,17 @@ import numpy as np
 def main():
     start = time.time()
     
-
-    f=open("../totalResults.csv","w")
     
-    for file in os.listdir("."):
+    
+    baseDir = sys.argv[1]
+    outFile = sys.argv[2]
+
+    f=open(outFile,"w")
+    
+    for file in os.listdir(baseDir):
         
         if "results" in file:
-            test = np.genfromtxt(file,int, delimiter = ",",usecols=(3,),invalid_raise=False)
+            test = np.genfromtxt(baseDir+file,int, delimiter = ",",usecols=(3,),invalid_raise=False)
             test = [i if i!=-1 else 10000 for i in test]
             f.write("%s,%f,%f\n" %(file.split(".")[0],np.mean(test),np.std(test)))
                         
---
+++ doCrittersExperiments.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+java -jar cycleTesting.jar 10 10 0 10000 1 500 resultsCritters/results10_10_0_10000_1.csv &
+java -jar cycleTesting.jar 10 10 0 10000 2 500 resultsCritters/results10_10_0_10000_2.csv &
+java -jar cycleTesting.jar 10 10 0 10000 3 500 resultsCritters/results10_10_0_10000_3.csv &
+java -jar cycleTesting.jar 10 10 0 10000 4 500 resultsCritters/results10_10_0_10000_4.csv 
+java -jar cycleTesting.jar 10 10 0 10000 5 500 resultsCritters/results10_10_0_10000_5.csv &
+java -jar cycleTesting.jar 10 10 0 10000 6 500 resultsCritters/results10_10_0_10000_6.csv &
+java -jar cycleTesting.jar 10 10 0 10000 7 500 resultsCritters/results10_10_0_10000_7.csv &
+java -jar cycleTesting.jar 10 10 0 10000 8 500 resultsCritters/results10_10_0_10000_8.csv 
+java -jar cycleTesting.jar 10 10 0 10000 9 500 resultsCritters/results10_10_0_10000_9.csv &
+java -jar cycleTesting.jar 10 10 0 10000 10 500 resultsCritters/results10_10_0_10000_10.csv &
+
+java -jar cycleTesting.jar 11 11 0 10000 1 500 resultsCritters/results11_11_0_10000_1.csv &
+java -jar cycleTesting.jar 11 11 0 10000 2 500 resultsCritters/results11_11_0_10000_2.csv 
+java -jar cycleTesting.jar 11 11 0 10000 3 500 resultsCritters/results11_11_0_10000_3.csv &
+java -jar cycleTesting.jar 11 11 0 10000 4 500 resultsCritters/results11_11_0_10000_4.csv &
+java -jar cycleTesting.jar 11 11 0 10000 5 500 resultsCritters/results11_11_0_10000_5.csv &
+java -jar cycleTesting.jar 11 11 0 10000 6 500 resultsCritters/results11_11_0_10000_6.csv
+java -jar cycleTesting.jar 11 11 0 10000 7 500 resultsCritters/results11_11_0_10000_7.csv &
+java -jar cycleTesting.jar 11 11 0 10000 8 500 resultsCritters/results11_11_0_10000_8.csv &
+java -jar cycleTesting.jar 11 11 0 10000 9 500 resultsCritters/results11_11_0_10000_9.csv &
+java -jar cycleTesting.jar 11 11 0 10000 10 500 resultsCritters/results11_11_0_10000_10.csv
+
+java -jar cycleTesting.jar 12 12 0 10000 1 500 resultsCritters/results12_12_0_10000_1.csv &
+java -jar cycleTesting.jar 12 12 0 10000 2 500 resultsCritters/results12_12_0_10000_2.csv &
+java -jar cycleTesting.jar 12 12 0 10000 3 500 resultsCritters/results12_12_0_10000_3.csv &
+java -jar cycleTesting.jar 12 12 0 10000 4 500 resultsCritters/results12_12_0_10000_4.csv
+java -jar cycleTesting.jar 12 12 0 10000 5 500 resultsCritters/results12_12_0_10000_5.csv &
+java -jar cycleTesting.jar 12 12 0 10000 6 500 resultsCritters/results12_12_0_10000_6.csv &
+java -jar cycleTesting.jar 12 12 0 10000 7 500 resultsCritters/results12_12_0_10000_7.csv &
+java -jar cycleTesting.jar 12 12 0 10000 8 500 resultsCritters/results12_12_0_10000_8.csv
+java -jar cycleTesting.jar 12 12 0 10000 9 500 resultsCritters/results12_12_0_10000_9.csv &
+java -jar cycleTesting.jar 12 12 0 10000 10 500 resultsCritters/results12_12_0_10000_10.csv &
+
+java -jar cycleTesting.jar 13 13 0 10000 1 500 resultsCritters/results13_13_0_10000_1.csv &
+java -jar cycleTesting.jar 13 13 0 10000 2 500 resultsCritters/results13_13_0_10000_2.csv
+java -jar cycleTesting.jar 13 13 0 10000 3 500 resultsCritters/results13_13_0_10000_3.csv &
+java -jar cycleTesting.jar 13 13 0 10000 4 500 resultsCritters/results13_13_0_10000_4.csv &
+java -jar cycleTesting.jar 13 13 0 10000 5 500 resultsCritters/results13_13_0_10000_5.csv &
+java -jar cycleTesting.jar 13 13 0 10000 6 500 resultsCritters/results13_13_0_10000_6.csv
+java -jar cycleTesting.jar 13 13 0 10000 7 500 resultsCritters/results13_13_0_10000_7.csv &
+java -jar cycleTesting.jar 13 13 0 10000 8 500 resultsCritters/results13_13_0_10000_8.csv &
+java -jar cycleTesting.jar 13 13 0 10000 9 500 resultsCritters/results13_13_0_10000_9.csv &
+java -jar cycleTesting.jar 13 13 0 10000 10 500 resultsCritters/results13_13_0_10000_10.csv
+
+java -jar cycleTesting.jar 14 14 0 10000 1 500 resultsCritters/results14_14_0_10000_1.csv &
+java -jar cycleTesting.jar 14 14 0 10000 2 500 resultsCritters/results14_14_0_10000_2.csv &
+java -jar cycleTesting.jar 14 14 0 10000 3 500 resultsCritters/results14_14_0_10000_3.csv &
+java -jar cycleTesting.jar 14 14 0 10000 4 500 resultsCritters/results14_14_0_10000_4.csv
+java -jar cycleTesting.jar 14 14 0 10000 5 500 resultsCritters/results14_14_0_10000_5.csv &
+java -jar cycleTesting.jar 14 14 0 10000 6 500 resultsCritters/results14_14_0_10000_6.csv &
+java -jar cycleTesting.jar 14 14 0 10000 7 500 resultsCritters/results14_14_0_10000_7.csv &
+java -jar cycleTesting.jar 14 14 0 10000 8 500 resultsCritters/results14_14_0_10000_8.csv
+java -jar cycleTesting.jar 14 14 0 10000 9 500 resultsCritters/results14_14_0_10000_9.csv &
+java -jar cycleTesting.jar 14 14 0 10000 10 500 resultsCritters/results14_14_0_10000_10.csv &
+
+java -jar cycleTesting.jar 15 15 0 10000 1 500 resultsCritters/results15_15_0_10000_1.csv &
+java -jar cycleTesting.jar 15 15 0 10000 2 500 resultsCritters/results15_15_0_10000_2.csv
+java -jar cycleTesting.jar 15 15 0 10000 3 500 resultsCritters/results15_15_0_10000_3.csv &
+java -jar cycleTesting.jar 15 15 0 10000 4 500 resultsCritters/results15_15_0_10000_4.csv &
+java -jar cycleTesting.jar 15 15 0 10000 5 500 resultsCritters/results15_15_0_10000_5.csv &
+java -jar cycleTesting.jar 15 15 0 10000 6 500 resultsCritters/results15_15_0_10000_6.csv
+java -jar cycleTesting.jar 15 15 0 10000 7 500 resultsCritters/results15_15_0_10000_7.csv &
+java -jar cycleTesting.jar 15 15 0 10000 8 500 resultsCritters/results15_15_0_10000_8.csv &
+java -jar cycleTesting.jar 15 15 0 10000 9 500 resultsCritters/results15_15_0_10000_9.csv &
+java -jar cycleTesting.jar 15 15 0 10000 10 500 resultsCritters/results15_15_0_10000_10.csv
