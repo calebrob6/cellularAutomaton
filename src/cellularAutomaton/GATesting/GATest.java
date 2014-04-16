d5ab3e674660759c2c2cc6aee4cd38f699496ecc
calebrob6 <calebrob6@gmail.com>
2014-04-15 19:08:40 -0500

    Doing work

--- src/cellularAutomaton/GATesting/GATest.java
+++ src/cellularAutomaton/GATesting/GATest.java
@@ -36,31 +36,18 @@ public class GATest {
 
 	public static EvolutionMonitor m_monitor;
 
-	public static void makeChangeForAmount(int a_targetChangeAmount,
-			boolean a_doMonitor) throws Exception {
-		// Start with a DefaultConfiguration, which comes setup with the
-		// most common settings.
-		// -------------------------------------------------------------
+	public static void makeChangeForAmount(int a_targetChangeAmount, boolean a_doMonitor) throws Exception {
+
 		Configuration conf = new DefaultConfiguration();
-		// Care that the fittest individual of the current population is
-		// always taken to the next generation.
-		// Consider: With that, the pop. size may exceed its original
-		// size by one sometimes!
-		// -------------------------------------------------------------
 		conf.setPreservFittestIndividual(true);
 		conf.setKeepPopulationSizeConstant(false);
-		// Set the fitness function we want to use, which is our
-		// MinimizingMakeChangeFitnessFunction. We construct it with
-		// the target amount of change passed in to this method.
-		// ---------------------------------------------------------
 		FitnessFunction myFunc = new GAFitness(a_targetChangeAmount);
 		conf.setFitnessFunction(myFunc);
 		if (a_doMonitor) {
-			// Turn on monitoring/auditing of evolution progress.
-			// --------------------------------------------------
 			m_monitor = new EvolutionMonitor();
 			conf.setMonitor(m_monitor);
 		}
+		
 		// Now we need to tell the Configuration object how we want our
 		// Chromosomes to be setup. We do that by actually creating a
 		// sample Chromosome and then setting it on the Configuration
@@ -72,44 +59,36 @@ public class GATest {
 		// also lets us specify a lower and upper bound, which we set
 		// to sensible values for each coin type.
 		// --------------------------------------------------------------
-		Gene[] sampleGenes = new Gene[4];
-		sampleGenes[0] = new IntegerGene(conf, 0, 10); // Quarters
-		sampleGenes[1] = new IntegerGene(conf, 0, 2); // Dimes
-		sampleGenes[2] = new IntegerGene(conf, 0, 1); // Nickels
-		sampleGenes[3] = new IntegerGene(conf, 0, 5); // Pennies
+		
+		Gene[] sampleGenes = new Gene[16];
+		for(int i=0;i<16;i++){
+			sampleGenes[i] = new IntegerGene(conf,0,15);
+		}
+		//sampleGenes[16] = new IntegerGene(conf, 0, 10000); // Number of iterations
+		//sampleGenes[17] = new IntegerGene(conf, 0, 50); // width
+		//sampleGenes[18] = new IntegerGene(conf, 0, 50); // height
+		
+		
+		
 		IChromosome sampleChromosome = new Chromosome(conf, sampleGenes);
+		
 		conf.setSampleChromosome(sampleChromosome);
-		// Finally, we need to tell the Configuration object how many
-		// Chromosomes we want in our population. The more Chromosomes,
-		// the larger number of potential solutions (which is good for
-		// finding the answer), but the longer it will take to evolve
-		// the population (which could be seen as bad).
-		// ------------------------------------------------------------
 		conf.setPopulationSize(20);
-
-		// Create random initial population of Chromosomes.
-		// Here we try to read in a previous run via XMLManager.readFile(..)
-		// for demonstration purpose only!
-		// -----------------------------------------------------------------
-		Genotype population;
+		
+		Genotype population = Genotype.randomInitialGenotype(conf);
+		
+		/*
 		try {
 			Document doc = XMLManager.readFile(new File("JGAPExample32.xml"));
 			population = XMLManager.getGenotypeFromDocument(conf, doc);
 		} catch (UnsupportedRepresentationException uex) {
-			// JGAP codebase might have changed between two consecutive runs.
-			// --------------------------------------------------------------
 			population = Genotype.randomInitialGenotype(conf);
 		} catch (FileNotFoundException fex) {
 			population = Genotype.randomInitialGenotype(conf);
 		}
-		// Now we initialize the population randomly, anyway (as an example
-		// only)!
-		// If you want to load previous results from file, remove the next line!
-		// -----------------------------------------------------------------------
-		population = Genotype.randomInitialGenotype(conf);
-		// Evolve the population. Since we don't know what the best answer
-		// is going to be, we just evolve the max number of times.
-		// ---------------------------------------------------------------
+		*/
+		
+
 		long startTime = System.currentTimeMillis();
 		for (int i = 0; i < MAX_ALLOWED_EVOLUTIONS; i++) {
 			if (!uniqueChromosomes(population.getPopulation())) {
@@ -122,48 +101,31 @@ public class GATest {
 			}
 		}
 		long endTime = System.currentTimeMillis();
-		System.out.println("Total evolution time: " + (endTime - startTime)
-				+ " ms");
+		System.out.println("Total evolution time: " + (endTime - startTime) + " ms");
+		
+		/*
 		// Save progress to file. A new run of this example will then be able to
 		// resume where it stopped before! --> this is completely optional.
-		// ---------------------------------------------------------------------
-
-		// Represent Genotype as tree with elements Chromomes and Genes.
-		// -------------------------------------------------------------
 		DataTreeBuilder builder = DataTreeBuilder.getInstance();
 		IDataCreators doc2 = builder.representGenotypeAsDocument(population);
-		// create XML document from generated tree
 		XMLDocumentBuilder docbuilder = new XMLDocumentBuilder();
 		Document xmlDoc = (Document) docbuilder.buildDocument(doc2);
 		XMLManager.writeFile(xmlDoc, new File("JGAPExample26.xml"));
-		// Display the best solution we found.
-		// -----------------------------------
+		*/
+		
+		
 		IChromosome bestSolutionSoFar = population.getFittestChromosome();
 		double v1 = bestSolutionSoFar.getFitnessValue();
-		System.out.println("The best solution has a fitness value of "
-				+ bestSolutionSoFar.getFitnessValue());
-		bestSolutionSoFar.setFitnessValueDirectly(-1);
-		System.out.println("It contains the following: ");
-		System.out.println("\t"
-				+ GAFitness.getNumberOfCoinsAtGene(bestSolutionSoFar, 0)
-				+ " quarters.");
-		System.out.println("\t"
-				+ GAFitness.getNumberOfCoinsAtGene(bestSolutionSoFar, 1)
-				+ " dimes.");
-		System.out.println("\t"
-				+ GAFitness.getNumberOfCoinsAtGene(bestSolutionSoFar, 2)
-				+ " nickels.");
-		System.out.println("\t"
-				+ GAFitness.getNumberOfCoinsAtGene(bestSolutionSoFar, 3)
-				+ " pennies.");
-		System.out.println("For a total of "
-				+ GAFitness.amountOfChange(bestSolutionSoFar) + " cents in "
-				+ GAFitness.getTotalNumberOfCoins(bestSolutionSoFar)
-				+ " coins.");
+		System.out.println("The best solution has a fitness value of " + v1);
+		//bestSolutionSoFar.setFitnessValueDirectly(-1);
+		System.out.println("Rule string: ");
+		for(int i=0;i<16;i++){
+			System.out.println(((Integer)bestSolutionSoFar.getGene(i).getAllele()).intValue() + ";");
+		}
+		
 	}
 
 	public static boolean uniqueChromosomes(Population a_pop) {
-		// Check that all chromosomes are unique
 		for (int i = 0; i < a_pop.size() - 1; i++) {
 			IChromosome c = a_pop.getChromosome(i);
 			for (int j = i + 1; j < a_pop.size(); j++) {
