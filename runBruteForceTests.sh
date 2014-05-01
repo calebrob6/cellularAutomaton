74b4bed290483849d10d29084e85f0ad2b846c8b
calebrob6 <calebrob6@gmail.com>
2014-05-01 14:15:43 -0500

    Working

---
+++ runBruteForceTests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+java -jar gaBruteForce.jar 3 3 2 2 resultsBF/3_3_2_2_test1.txt &
+java -jar gaBruteForce.jar 3 3 2 2 resultsBF/3_3_2_2_test2.txt &
+java -jar gaBruteForce.jar 3 3 2 2 resultsBF/3_3_2_2_test3.txt &
+java -jar gaBruteForce.jar 3 3 2 2 resultsBF/3_3_2_2_test4.txt &
+
+java -jar gaBruteForce.jar 3 3 2 2 resultsBF/3_3_2_2_test5.txt &
+java -jar gaBruteForce.jar 3 3 2 2 resultsBF/3_3_2_2_test6.txt &
+java -jar gaBruteForce.jar 3 3 2 2 resultsBF/3_3_2_2_test7.txt &
+java -jar gaBruteForce.jar 3 3 2 2 resultsBF/3_3_2_2_test8.txt &
+#java -jar gaBruteForce.jar 4 4 2 2 resultsBF/4_4_2_2_test1.txt &
+#java -jar gaBruteForce.jar 4 4 2 2 resultsBF/4_4_2_2_test2.txt &
+#java -jar gaBruteForce.jar 4 4 2 2 resultsBF/4_4_2_2_test3.txt & 
+#java -jar gaBruteForce.jar 4 4 2 2 resultsBF/4_4_2_2_test4.txt &
+
